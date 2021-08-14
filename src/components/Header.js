458efc30b3eb2ea7ef8660a62d89237898600396
ggrossvi <hello.gloria@gmail.com>
2021-08-14 00:46:29 -0700

    added header and link style

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <ul className="navigation-menu">
+      <li>
+        <NavLink to="/" activeClassName="active">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/login" activeClassName="active">LogIn</NavLink>
+      </li>
+      <li>
+        <NavLink to="/register" activeClassName="active">Register</NavLink>
+      </li>
+      <li>
+        <NavLink to="/edit" activeClassName="active">Edit</NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from "./components/Header";
+import './index.css';
+
+const Home = () => {
+  return <h2>Home</h2>
+};
+
+const LogIn = () => {
+  return <h2>LogIn</h2>
+};
+
+const Register = () => {
+  return <h2>Register</h2>
+};
+
+const Buddyup = () => {
+  return <h2>Buddyup</h2>
+};
+
+const Edit = () => {
+  return <h2>Edit</h2>
+};
+
+const NotFound = () => {
+  return <h2>Page Not found.</h2>;
+};
+
+const App = () => {
+  return (
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/" component={Home} exact = {true} />
+          <Route path="/login" component={LogIn} />
+          <Route path="/register" component={Register} />
+          <Route path="/buddyup" component={Buddyup} />
+          <Route path="/edit" component={Edit} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <h1>Welcome to Walking Buddy</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +58,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
