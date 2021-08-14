1ccdbe61e6a38cea0b2ec0dcbcd4c99ae0e320c2
ggrossvi <hello.gloria@gmail.com>
2021-08-14 11:16:14 -0700

    added import router, route and swithch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Register from './components/Register';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
-      <Register />
-    </div>
+    <Router>
+      <div className="App">
+        <Register />
+      </div>
+    </Router>
   );
 }
 
