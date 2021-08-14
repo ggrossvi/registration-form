7413e0dfa137171d8704d2af7f01ee3b43a94609
ggrossvi <hello.gloria@gmail.com>
2021-08-14 00:50:33 -0700

    fixed home navigation highlighting

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <ul className="navigation-menu">
       <li>
-        <NavLink to="/" activeClassName="active">Home</NavLink>
+        <NavLink to="/" activeClassName="active" exact={true}>Home</NavLink>
       </li>
       <li>
         <NavLink to="/login" activeClassName="active">LogIn</NavLink>
