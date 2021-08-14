8dd37a6d7cd227d4373d0411892874a682da28d1
ggrossvi <hello.gloria@gmail.com>
2021-08-14 15:52:48 -0700

    added remaining needed fields to update form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Register from './components/Register';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-
+console.log(process.env.REACT_APP_GOOGLE_API_KEY)
 
 function App() {
   return (
