3bdae2c861d9d19b90292e21d407a8164c460ded
ggrossvi <hello.gloria@gmail.com>
2021-08-20 16:38:02 -0700

    fixed problem with map - zoom was too big showed markers on top of each other

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,54 +3,62 @@
 //https://tintef.github.io/react-google-places-autocomplete/docs/
 //https://github.com/istarkov/google-map-clustering-example
 
-import { GoogleMap, withScriptsjs, Marker, withGoogleMap, InfoWindow } from "react-google-maps";
+import {
+  GoogleMap,
+  withScriptsjs,
+  Marker,
+  withGoogleMap,
+  InfoWindow,
+} from "react-google-maps";
 import * as buddyData from "./buddy.json";
 // import mapStyles from "./mapStyles";
 // import nodeExternals from 'webpack-node-externals';
 import React, { useState, useEffect } from "react";
 
-
-
 function Map() {
-    const [selectedBuddy, setSelectedBuddy] = useState(null);
-    return (
-        <GoogleMap
-            defaultZoom={11}
-            defaultCenter={{ lat: 47.6062, lng: -122.3321}}
-        >
-            {buddyData.buddies.map(buddy => (
-                <Marker
-                    key={buddy.properties.BUDDY_ID}
-                    position={{
-                        lat: buddy.geometry.coordinates[0],
-                        
-                        lng: buddy.geometry.coordinates[1]
-                    }}
-            
-                    onClick={() => { setSelectedBuddy(buddy); }}
-                    // icon={{
-                    //     url: `/skateboarding.svg`,
-                    //     scaledSize: new window.google.maps.Size(25, 25)
-                    // }}
-                />
-            ))}
+  const [selectedBuddy, setSelectedBuddy] = useState(null);
 
-            {selectedBuddy && (
-                <InfoWindow
-                    onCloseClick={() => { setSelectedBuddy(null); }}
-                    position={{
-                        lat: selectedBuddy.geometry.coordinates[0],
-                        lng: selectedBuddy.geometry.coordinates[1]
-                    }}
-                >
-                    <div>
-                        <h2>{selectedBuddy.properties.NAME}</h2>
-                        <p>{selectedBuddy.properties.DESCRIPTION}</p>
-                    </div>
-                </InfoWindow>
-            )}
-        </GoogleMap>
-    );
+  return (
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 47.6062, lng: -122.3321 }}
+    >
+      {buddyData.buddies.map((buddy) => (
+        <Marker
+          key={buddy.properties.BUDDY_ID}
+          position={{
+            lat: buddy.geometry.coordinates[0],
+
+            lng: buddy.geometry.coordinates[1],
+          }}
+          onClick={() => {
+            setSelectedBuddy(buddy);
+          }}
+          // icon={{
+          //     url: `/skateboarding.svg`,
+          //     scaledSize: new window.google.maps.Size(25, 25)
+          // }}
+        />
+      ))}
+
+      {selectedBuddy && (
+        <InfoWindow
+          onCloseClick={() => {
+            setSelectedBuddy(null);
+          }}
+          position={{
+            lat: selectedBuddy.geometry.coordinates[0],
+            lng: selectedBuddy.geometry.coordinates[1],
+          }}
+        >
+          <div>
+            <h2>{selectedBuddy.properties.NAME}</h2>
+            <p>{selectedBuddy.properties.DESCRIPTION}</p>
+          </div>
+        </InfoWindow>
+      )}
+    </GoogleMap>
+  );
 }
 
 export default Map;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,47 +1,44 @@
 import React, { useState, useEffect } from "react";
 // import { times } from "../utils/times";
-import axios from 'axios';
-import BuddyList from './BuddyList';
-import Card from 'react-bootstrap/Card';
-import Map from './Map';
-import {GoogleMap, withScriptjs, withGoogleMap} from "react-google-maps"
+import axios from "axios";
+import BuddyList from "./BuddyList";
+import Card from "react-bootstrap/Card";
+import Map from "./Map";
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
 // import './src/index.css'
-
-
 
 const baseURL = "http://localhost:5000";
 const WrappedMap = withScriptjs(withGoogleMap(Map));
 const times = [
   {
-      time: "Morning"
+    time: "Morning",
   },
   {
-      time: "Afternoon"
+    time: "Afternoon",
   },
   {
-      time: "Evening"
-  }
-]
-
+    time: "Evening",
+  },
+];
 
 function Register() {
-  console.log("i am here")
-  console.log(process.env.REACT_APP_GOOGLE_KEY)
-  const [lookupEmail, setLookupEmail] = useState('');
-  const [fname, setFname] = useState('');
-  const[lname, setLname] = useState('');
-  const [email, setEmail] = useState('');
-  const [address, setAddress] = useState('');
-  const [apt, setApt] = useState('');
-  const [city, setCity] = useState('');
-  const [state, setState] = useState('');
-  const [zipcode, setZipCode] = useState('');
-  const [bio, setBio] = useState('');
+  console.log("i am here");
+  console.log(process.env.REACT_APP_GOOGLE_KEY);
+  const [lookupEmail, setLookupEmail] = useState("");
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [apt, setApt] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [zipcode, setZipCode] = useState("");
+  const [bio, setBio] = useState("");
   //Checkboxes hold time preference for walks
   const [checkboxes, setCheckboxes] = useState([false, false, false]);
-  const [buddyData,setBuddyData] = useState([]);
-  const [isExisting,setIsExisting] = useState(false);
- 
+  const [buddyData, setBuddyData] = useState([]);
+  const [isExisting, setIsExisting] = useState(false);
+
   // const [post, setPost] = React.useState(null);
 
   // React.useEffect(() => {
@@ -51,7 +48,7 @@ function Register() {
   // }, []);
 
   function handleGetRecords() {
-  //update below
+    //update below
     axios
       .post("/buddy/register", {
         // given_name and family_name have this because of login object having these names.
@@ -71,13 +68,14 @@ function Register() {
         morning: checkboxes[0],
         afternoon: checkboxes[1],
         evening: checkboxes[2],
-        bio: bio
+        bio: bio,
       })
       .then((response) => {
-        console.log("it worked!")
-        console.log(response)
-        setBuddyData(response.data)
-      }).catch((err) => console.log(err));
+        console.log("it worked!");
+        console.log(response);
+        setBuddyData(response.data);
+      })
+      .catch((err) => console.log(err));
   }
 
   function getRecordByEmail(event) {
@@ -85,16 +83,17 @@ function Register() {
     axios
       .get(`/buddy/email/${lookupEmail}`)
       .then((response) => {
-        console.log("it worked!")
-        console.log(response.data)
-        const record = response.data[0]
+        console.log("it worked!");
+        console.log(response.data);
+        const record = response.data[0];
 
-        setEmail(record.email)
+        setEmail(record.email);
         //if it's true then they can't edit email
-        setIsExisting(true)
+        setIsExisting(true);
         // Put an error handler here
         // if response.data
-      }).catch((err) => console.log(err));
+      })
+      .catch((err) => console.log(err));
   }
 
   function createPost(event) {
@@ -118,13 +117,14 @@ function Register() {
         morning: checkboxes[0],
         afternoon: checkboxes[1],
         evening: checkboxes[2],
-        bio: bio
+        bio: bio,
       })
       .then((response) => {
-        console.log("it worked!")
-        console.log(response)
-        setBuddyData(response.data)
-      }).catch((err) => console.log(err));
+        console.log("it worked!");
+        console.log(response);
+        setBuddyData(response.data);
+      })
+      .catch((err) => console.log(err));
   }
 
   // if (!post) {
@@ -132,61 +132,56 @@ function Register() {
   //   return "No post!"
   // };
 
-  const handleLookupEmailChange = event => {
-    setLookupEmail(event.target.value)
+  const handleLookupEmailChange = (event) => {
+    setLookupEmail(event.target.value);
   };
-  const handleLnameChange = event => {
-    setLname(event.target.value)
+  const handleLnameChange = (event) => {
+    setLname(event.target.value);
   };
-  const handleFnameChange = event => {
-    setFname(event.target.value)
+  const handleFnameChange = (event) => {
+    setFname(event.target.value);
   };
-  const handleEmailChange = event => {
-    setEmail(event.target.value)
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
   };
-  const handleAddressChange = event => {
-    setAddress(event.target.value)
+  const handleAddressChange = (event) => {
+    setAddress(event.target.value);
   };
-  const handleAptChange = event => {
-    setApt(event.target.value)
+  const handleAptChange = (event) => {
+    setApt(event.target.value);
   };
-  const handleCityChange = event => {
-    setCity(event.target.value)
+  const handleCityChange = (event) => {
+    setCity(event.target.value);
   };
-  const handleStateChange = event => {
-    setState(event.target.value)
+  const handleStateChange = (event) => {
+    setState(event.target.value);
   };
-  const handleZipcodeChange = event => {
-    setZipCode(event.target.value)
+  const handleZipcodeChange = (event) => {
+    setZipCode(event.target.value);
   };
-  const handleBioChange = event => {
-    setBio(event.target.value)
+  const handleBioChange = (event) => {
+    setBio(event.target.value);
   };
-  
+
   const handleCheckboxChange = (event, index) => {
     checkboxes[index] = event.target.checked;
     setCheckboxes(checkboxes);
-}
-
- 
+  };
 
   // const handleSubmit = event => {
   //   event.preventDefault();
   //    {/*lname: ${lname} \n */}
   //    {/*password: ${password} \n*/}
-  //   alert(`Your state values: \n         
-  //           fname: ${fname} \n 
-  //           zip: ${zipcode} \n 
-  //           email: ${email} \n          
+  //   alert(`Your state values: \n
+  //           fname: ${fname} \n
+  //           zip: ${zipcode} \n
+  //           email: ${email} \n
   //           You can replace this alert with your process`);
   // };
 
-  
-
   return (
     <div>
-    
-    {/* <form onSubmit={getRecordByEmail}>
+      {/* <form onSubmit={getRecordByEmail}>
        
         <div>
             <label>Email address</label>
@@ -203,156 +198,149 @@ function Register() {
       <button type="submit">GET RECORD</button>
     </form> */}
 
-<p>
-    <section>
-    
-      <div style= {{width:'100vw', height: '100vh'}}>
-     
-      <WrappedMap 
-           googleMapURL= {`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCalnnIIvWiASRzEqQPSaa6u-5xii3IRQI`}
-         
-          
-          loadingElement={<div style={{ height: `90%` }} />}
-          containerElement={<div style={{ height: `90%` }} />}
-          mapElement={<div style={{ height: `100%` }} />}          
-        />
-       
-      </div>
-      
-      </section>
-      </p> 
-
-
-    <form onSubmit={createPost}>
-        <div>
+      <p>
+        <section>
+          <div style={{ width: "100vw", height: "100vh" }}>
+            <WrappedMap
+              googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCalnnIIvWiASRzEqQPSaa6u-5xii3IRQI`}
+              loadingElement={<div style={{ height: `100%` }} />}
+              containerElement={<div style={{ height: `100%` }} />}
+              mapElement={<div style={{ height: `100%` }} />}
+            />
+          </div>
+        </section>
+      </p>
+      <div className="register-form-div">
+        <form className="register-form" onSubmit={createPost}>
+          <div className="form-input">
             <label>First name</label>
             <input
-            type="fname"
-            name="fname"
-            placeholder="Enter first name"
-            onChange={handleFnameChange}
-            value={fname}
+              type="fname"
+              name="fname"
+              placeholder="Enter first name"
+              onChange={handleFnameChange}
+              value={fname}
             />
-      </div>
-       <div>
+          </div>
+          <div className="form-input">
             <label>Last name</label>
             <input
-            type="lname"
-            name="lname"
-            placeholder="Enter last name"
-            onChange={handleLnameChange}
-            value={lname}
+              type="lname"
+              name="lname"
+              placeholder="Enter last name"
+              onChange={handleLnameChange}
+              value={lname}
             />
-        </div> 
-        <div>
+          </div>
+          <div className="form-input">
             <label>Email address</label>
             <input
-            type="email"
-            name="email"
-            placeholder="Enter email"
-            onChange={handleEmailChange}
-            value={email}
-            disabled={isExisting}
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              onChange={handleEmailChange}
+              value={email}
+              disabled={isExisting}
             />
-        </div>
-        <div>
+          </div>
+          <div className="form-input">
             <label>Address</label>
             <input
-            type="address"
-            name="address"
-            placeholder="Enter address"
-            onChange={handleAddressChange}
-            value={address}
+              type="address"
+              name="address"
+              placeholder="Enter address"
+              onChange={handleAddressChange}
+              value={address}
             />
-        </div>
-        <div>
+          </div>
+          <div className="form-input">
             <label>Apt</label>
             <input
-            type="apt"
-            name="apt"
-            placeholder="Enter apt"
-            onChange={handleAptChange}
-            value={apt}
+              type="apt"
+              name="apt"
+              placeholder="Enter apt"
+              onChange={handleAptChange}
+              value={apt}
             />
-        </div>
-        <div>
+          </div>
+          <div className="form-input">
             <label>City</label>
             <input
-            type="city"
-            name="city"
-            placeholder="Enter city"
-            onChange={handleCityChange}
-            value={city}
+              type="city"
+              name="city"
+              placeholder="Enter city"
+              onChange={handleCityChange}
+              value={city}
             />
-        </div>
-        <div>
+          </div>
+          <div className="form-input">
             <label>State</label>
             <input
-            type="state"
-            name="state"
-            placeholder="Enter state"
-            onChange={handleStateChange}
-            value={state}
+              type="state"
+              name="state"
+              placeholder="Enter state"
+              onChange={handleStateChange}
+              value={state}
             />
-        </div>
+          </div>
 
-        <div>
+          <div className="form-input">
             <label>Zip</label>
             <input
-            type="zip"
-            name="zipcode"
-            placeholder="Enter zip"
-            onChange={handleZipcodeChange}
-            value={zipcode}
+              type="zip"
+              name="zipcode"
+              placeholder="Enter zip"
+              onChange={handleZipcodeChange}
+              value={zipcode}
             />
-        </div>
-        <div>
+          </div>
+          <div className="form-input">
             <label>Bio</label>
             <input
-            type="bio"
-            name="bio"
-            placeholder="Enter bio"
-            onChange={handleBioChange}
-            value={bio}
+              type="bio"
+              name="bio"
+              placeholder="Enter bio"
+              onChange={handleBioChange}
+              value={bio}
             />
-        </div>
+          </div>
 
-     
-
-        <h3>Select Times</h3>
-            <ul className="times-list">
-                {times.map(({time}, index) => (
-        
-                <li key={index}>
+          <h3>Select Times</h3>
+          <ul className="times-list">
+            {times.map(({ time }, index) => (
+              <li key={index}>
                 <div className="times-list-item">
-                    <div className="left-section">
-                        <input
-                            type="checkbox"
-                            id={`custom-checkbox-${index}`}
-                            name={time}
-                            onChange={(event) => handleCheckboxChange(event, index)}
-                        />
-                        <label htmlFor={`custom-checkbox-${index}`}>{time}</label>
-                    </div>      
+                  <div className="left-section">
+                    <input
+                      type="checkbox"
+                      id={`custom-checkbox-${index}`}
+                      name={time}
+                      onChange={(event) => handleCheckboxChange(event, index)}
+                    />
+                    <label htmlFor={`custom-checkbox-${index}`}>{time}</label>
+                  </div>
                 </div>
-                </li>
-                )
-                )
-            }
-            </ul>
-          
-      <button type="submit">Submit</button>
-      {/*if first thing thing is true it will return the 2nd
+              </li>
+            ))}
+          </ul>
+
+          <button type="submit">Submit</button>
+          {/*if first thing thing is true it will return the 2nd
       Write custome onClick function so it 
       Declare function above api call 
       */}
-      {isExisting && <button onClick = {handleGetRecords}>Get Records</button>}
-    </form>
-    <br/>
-    <section>
-    <BuddyList buddy_data = {buddyData}/><br/>
-    </section>
-    {/* <p>
+          {isExisting && (
+            <button onClick={handleGetRecords}>Get Records</button>
+          )}
+        </form>
+      </div>
+
+      <br />
+      <section>
+        <BuddyList buddy_data={buddyData} />
+        <br />
+      </section>
+      {/* <p>
     <section>
     
       <div style= {{width:'100vw', height: '100vh'}}>
@@ -370,11 +358,8 @@ function Register() {
       
       </section>
       </p>  */}
-
-    </div >
-    
-  )
+    </div>
+  );
 }
-
 
 export default Register;
